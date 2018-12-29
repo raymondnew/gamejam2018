c7cceba1ff686a1b271fc62d2fed27aa6a0ab4c5
raymondnew <raymondnew@gmail.com>
2018-12-28 16:05:03 -0800

    Game Camera rotation

--- trunk/Assets/Scripts/Base/GameCamera.cs
+++ trunk/Assets/Scripts/Base/GameCamera.cs
@@ -8,6 +8,9 @@ public class GameCamera : MonoBehaviour
     [SerializeField]
     float m_CameraMovementSpeed = 5.0f;
 
+    [SerializeField]
+    float m_CameraRotateSpeed = 5.0f;
+
     private Camera m_ThisCamera;
 
     void Awake()
@@ -24,19 +27,47 @@ public class GameCamera : MonoBehaviour
     void Update()
     {
         HandleTranslation();
+        HandleRotation();
     }
 
     void HandleTranslation()
     {
         Vector3 camPosition = transform.position;
 
-        float camX = camPosition.x;
-        float camZ = camPosition.z;
+        float camZ = Input.GetAxis("Horizontal") * Time.deltaTime * m_CameraMovementSpeed;
+        float camX = Input.GetAxis("Vertical") * Time.deltaTime * m_CameraMovementSpeed;
 
-        camX += Input.GetAxis("Horizontal") * Time.deltaTime * m_CameraMovementSpeed;
-        camZ += Input.GetAxis("Vertical") * Time.deltaTime * m_CameraMovementSpeed;
+        Vector3 forwardDir = (new Vector3(transform.forward.x, 0.0f, transform.forward.z)).normalized;
+        Vector3 newCamPosition = camPosition + (forwardDir * camX);
+        newCamPosition += transform.right * camZ;
+        transform.position = newCamPosition;
+    }
 
-        camPosition = new Vector3(camX, camPosition.y, camZ);
-        transform.position = camPosition;
+    void HandleRotation()
+    {
+        float rotation = 0.0f;
+        if (Input.GetKey(KeyCode.Q))
+            rotation++;
+        if (Input.GetKey(KeyCode.E))
+            rotation--;
+
+        transform.RotateAround(GetCamLookPoint(), Vector3.up, rotation * Time.deltaTime * m_CameraRotateSpeed);
+    }
+
+    Vector3 GetCamLookPoint()
+    {
+        float angle = Vector3.Angle(transform.forward, Vector3.forward);
+        //Debug.Log("Cam angle: " + angle);
+
+        float length = Mathf.Atan(angle * Mathf.Deg2Rad) * transform.position.y;
+        //Debug.Log("Cam length: " + length);
+
+        float distance = Mathf.Sqrt((length * length) + (transform.position.y * transform.position.y));
+        //Debug.Log("Cam distance: " + distance);
+
+        Vector3 lookPoint = transform.position + (transform.forward * distance);
+        //Debug.Log("Cam look point: " + lookPoint);
+
+        return lookPoint;
     }
 }
